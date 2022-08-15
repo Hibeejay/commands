873c2ce80cef9e6a55b177595663c8cd0d6695a9
Hibeejay <110846682+Hibeejay@users.noreply.github.com>
2022-08-15 19:46:01 +0100

    Update and rename DevCryptoWalletBot.js

--- libs/DevCryptoWalletBot.js
+++ libs/DevCryptoWalletBot.js
@@ -1,7 +1,7 @@
-let libPrefix = "CryptoAdGateWayBotLib"
+let libPrefix = "DevCryptoWalletBotLib"
 let lib = {
   endpoint:
-    "https://api.bots.business/v1/bots/721985/new-webhook?&command=connect&public_user_token=5e81ca1129efa83d2142f19bba0db32d&user_id=8785339",
+    "https://api.bots.business/v1/bots/703862/new-webhook?&command=connect%26transaction&public_user_token=14b4cb728407f20f080de711d113cf97&user_id=1949188",
   commands: {
     onNotification: libPrefix + "_onNotification"
   },
@@ -10,15 +10,15 @@ let lib = {
 
 function setupAdminPanel() {
   var panel = {
-    title: "CryptoAdGateWayBot options",
-    description: "Options for CryptoAdGateWayBot Lib",
+    title: "DevCryptoWalletBot options",
+    description: "Options for DevCryptoWalletBot Lib",
     icon: "logo-bitcoin",
 
     fields: [
       {
         name: "APIKey",
         title: "API Key",
-        description: "you can get your API key in @CryptoAdGateWayBot",
+        description: "you can get your API key in @DevCryptoWalletBot",
         type: "password",
         placeholder: "API Key",
         icon: "key"
@@ -26,7 +26,7 @@ function setupAdminPanel() {
       {
         name: "SecretAPIKey",
         title: "Secret API Key",
-        description: "you can get your Secret API key in @CryptoAdGateWayBot",
+        description: "you can get your Secret API key in @DevCryptoWalletBot",
         type: "password",
         placeholder: "Secret API Key",
         icon: "key"
